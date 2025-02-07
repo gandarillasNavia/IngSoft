1d531cd8a7440262f711a522cd2a4cbcee22e558
Mateo Gandarillas <mateoganav@gmail.com>
2025-02-07 17:33:11 -0400

    Cambios de nombre de variable y funcion

--- main.cpp
+++ main.cpp
@@ -2,32 +2,32 @@
 #include<locale.h>
 using namespace std;
 
-void Promedio(int nota[],int n,int &may){
-int total=0,prom;
-cout<<"Ingrese las notas"<<endl;
-for(int i=1;i<=n;i++){
-    cout<<"Estudiante ["<<i<<"] = ";
-    cin>>nota[i];
-    total=total+nota[i];
-}
-prom=total/n;
-cout<<"El promedio es "<<prom<<endl;
-for(int i=1;i<=n;i++){
- if(nota[i]>prom)
-    {
-        may++;
+int Promedio(int nota[],int n){
+    int total=0,prom,aprobados ;
+    cout<<"Ingrese las notas"<<endl;
+    for(int i=1;i<=n;i++){
+        cout<<"Estudiante ["<<i<<"] = ";
+        cin>>nota[i];
+        total=total+nota[i];
     }
-}
-
+    prom=total/n;
+    cout<<"El promedio es "<<prom<<endl;
+    for(int i=1;i<=n;i++){
+         if(nota[i]>prom)
+         {
+            aprobados++;
+         }
+    }
+    return aprobados;
 }
 
 int main()
 {setlocale(LC_ALL,"spanish");
 int nota[50];
-int n,may=0;
+int n,aprobadosT=0;
     cout<<"¿Cantidad de alumnos?"<<endl;
     cin>>n;
-    Promedio(nota,n,may);
-    cout<<"El numero de estudiantes aprobados es "<<may<<endl;
+    aprobadosT = Promedio(nota,n);
+    cout<<"El numero de estudiantes aprobados es "<<aprobadosT<<endl;
     return 0;
 }
